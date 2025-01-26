af404d47f9b2d50f76db6555e64b7a5d0e6c603f
Vlora04 <vloraabllaca@gmail.com>
2025-01-25 19:21:17 -1200

    added the slider

--- script.js
+++ script.js
@@ -3,10 +3,38 @@ function toggleMenu() {
     navLinks.classList.toggle('show');
 }
 
-// Kontrolloni madhësinë e ekranit kur ndryshon
 window.addEventListener('resize', () => {
     const navLinks = document.querySelector('.nav-links');
     if (window.innerWidth >= 768) {
-        navLinks.classList.remove('show'); // Hiq "show" për desktop
+        navLinks.classList.remove('show'); 
     }
 });
+const heroSection = document.querySelector('.hero');
+
+
+const backgrounds = [
+  'Images/pic1.jpg',
+  'Images/pic27.jpg',
+  'Images/pic25.jpg'
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+  heroSection.style.backgroundImage = `url('${backgrounds[currentIndex]}')`;
+}
+
+function nextBackground() {
+  currentIndex = (currentIndex + 1) % backgrounds.length;
+  changeBackground();
+}
+
+function prevBackground() {
+  currentIndex = (currentIndex - 1 + backgrounds.length) % backgrounds.length;
+  changeBackground();
+}
+
+document.querySelector('.next-btn').addEventListener('click', nextBackground);
+document.querySelector('.prev-btn').addEventListener('click', prevBackground);
+
+changeBackground();
