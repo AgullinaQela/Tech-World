995550b587b78afafb9e2b3222694924e7a2431a
Vlora04 <vloraabllaca@gmail.com>
2025-01-29 01:23:51 -1200

    added database.php

--- Database.php
+++ Database.php
@@ -1,16 +1,16 @@
 <?php
 class Database {
-    private $host = 'localhost'; // Adresa e serverit (zakonisht localhost)
-    private $dbname = 'techworldproject'; // Emri i bazës së të dhënave
-    private $username = 'root'; // Përdoruesi (default për MySQL është "root")
-    private $password = ''; // Fjalëkalimi (default për MySQL është bosh)
+    private $host = 'localhost'; 
+    private $dbname = 'techworldproject'; 
+    private $username = 'root'; 
+    private $password = ''; 
     private $conn;
 
     public function __construct() {
         try {
-            // Krijimi i lidhjes me PDO
+            
             $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Aktivizon raportimin e gabimeve
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
